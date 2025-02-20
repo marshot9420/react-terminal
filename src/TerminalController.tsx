49329497fb2c71348c727f4fcaedea45757fde1a
marshot9420 <marshot9420@gmail.com>
2025-02-21 06:47:28 +0900

    deps: react-terminal-ui 의존성 추가

---
+++ src/TerminalController.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Terminal, { ColorMode, TerminalOutput } from "react-terminal-ui";
+
+const TerminalController = () => {
+  const [terminalLineData, setTerminalLineData] = useState([
+    <TerminalOutput key="welcome">
+      <h1>Advanced Awareness Assessment</h1>
+      <p>Lo-Fi Operation Aptitude Test</p>
+      <p>Please Login with your username and password.</p>
+    </TerminalOutput>,
+  ]);
+
+  return (
+    <div className="terminal-container">
+      <Terminal
+        name="React Terminal Usage Example"
+        colorMode={ColorMode.Light}
+        onInput={(terminalInput) =>
+          console.log(`New terminal input received: '${terminalInput}'`)
+        }
+      >
+        {terminalLineData}
+      </Terminal>
+    </div>
+  );
+};
+
+export default TerminalController;
